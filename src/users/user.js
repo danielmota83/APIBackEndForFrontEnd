e3e83abdcf78b5e38817e7c486a41c56f89f66cd
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-07-12 14:49:26 -0300

    chore: create userjs

---
+++ src/users/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs")
+
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true, select: false },
+  profileImage: { type: String },
+});
+
+UserSchema.pre("save", async function(next) {
+  this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+const User = mongoose.model("User", UserSchema, "users");
+
+module.exports = User;
