f4cefaddbfc8341d94f852b05c80d22b9ed07446
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-07-12 15:17:21 -0300

    chore: adding routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 const express =require('express');
 
-const port = 3000
+const port = process.env.PORT || 3000;
 
 const app = express();
 
+connectToDatabase();
+
 app.use(cors());
 app.use(express.json());
+
+app.use('/users', userRoute);
+app.use('/auth', authRoute);
+app.use('/characters', characterRoute);
+app.use("/api-docs", swaggerRoute);
 
 app.listen(port, ()=>{
     console.log(`Servidor rodando na porta ${port}`);
